4bb084c060055f3dbecf15a629f86edd9989fe7d
Arkadiusz Chmura <arekchmura@gmail.com>
2019-01-01 15:09:47 +0100

    removed unnecessary package names

--- app/src/main/java/com/arkadiusz/dayscounter/adapters/ViewPagerAdapter.kt
+++ app/src/main/java/com/arkadiusz/dayscounter/adapters/ViewPagerAdapter.kt
@@ -4,12 +4,12 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 
-class ViewPagerAdapter(manager: androidx.fragment.app.FragmentManager) : androidx.fragment.app.FragmentPagerAdapter(manager) {
+class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
 
-    private val fragmentList = mutableListOf<androidx.fragment.app.Fragment>()
+    private val fragmentList = mutableListOf<Fragment>()
     private val fragmentTitleList = mutableListOf<String>()
 
-    override fun getItem(position: Int): androidx.fragment.app.Fragment {
+    override fun getItem(position: Int): Fragment {
         return fragmentList[position]
     }
 
@@ -21,7 +21,7 @@ class ViewPagerAdapter(manager: androidx.fragment.app.FragmentManager) : android
         return fragmentTitleList[position]
     }
 
-    fun addFragment(fragment: androidx.fragment.app.Fragment, title: String) {
+    fun addFragment(fragment: Fragment, title: String) {
         fragmentList.add(fragment)
         fragmentTitleList.add(title)
     }
