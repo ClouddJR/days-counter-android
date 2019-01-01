da913cb9bad5285834ee8fb592e940900b8ebc12
Arkadiusz Chmura <arekchmura@gmail.com>
2019-01-01 14:50:56 +0100

    finishing settings activity after back button press

--- app/src/main/java/com/arkadiusz/dayscounter/settings/SettingsActivity.kt
+++ app/src/main/java/com/arkadiusz/dayscounter/settings/SettingsActivity.kt
@@ -1,8 +1,11 @@
 package com.arkadiusz.dayscounter.settings
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.arkadiusz.dayscounter.R
+import com.arkadiusz.dayscounter.activities.MainActivity
+import org.jetbrains.anko.startActivity
 
 
 class SettingsActivity : AppCompatActivity() {
@@ -15,5 +18,20 @@ class SettingsActivity : AppCompatActivity() {
                 .beginTransaction()
                 .replace(R.id.settings_container, SettingsFragment())
                 .commit()
+    }
+
+    override fun onBackPressed() {
+        startActivity<MainActivity>()
+        finish()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                startActivity<MainActivity>()
+                finish()
+            }
+        }
+        return true
     }
 }
