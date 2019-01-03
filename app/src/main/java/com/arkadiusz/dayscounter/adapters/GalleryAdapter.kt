4193bc4fed1c8d2ed52e8ac2497bc522e8820bfb
Arkadiusz Chmura <arekchmura@gmail.com>
2019-01-03 19:58:47 +0100

    removing unnecessary packages

--- app/src/main/java/com/arkadiusz/dayscounter/adapters/GalleryAdapter.kt
+++ app/src/main/java/com/arkadiusz/dayscounter/adapters/GalleryAdapter.kt
@@ -1,10 +1,10 @@
 package com.arkadiusz.dayscounter.adapters
 
 import android.content.Context
-import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.arkadiusz.dayscounter.R
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.single_image_gallery.view.*
@@ -13,7 +13,7 @@ import kotlinx.android.synthetic.main.single_image_gallery.view.*
  * Created by arkadiusz on 11.03.18
  */
 
-class GalleryAdapter(val imagesList: IntArray, val context: Context) : androidx.recyclerview.widget.RecyclerView.Adapter<GalleryAdapter.ViewHolder>() {
+class GalleryAdapter(val imagesList: IntArray, val context: Context) : RecyclerView.Adapter<GalleryAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.single_image_gallery, parent, false)
@@ -24,7 +24,7 @@ class GalleryAdapter(val imagesList: IntArray, val context: Context) : androidx.
 
     override fun getItemCount(): Int = imagesList.size
 
-    inner class ViewHolder(itemView: View) : androidx.recyclerview.widget.RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         fun displayImage(position: Int) {
             Picasso.with(context).load(imagesList[position]).resize(0, 300).into(itemView.galleryImage)
