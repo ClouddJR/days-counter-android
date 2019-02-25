3866c3cb6e51f305ac59b0bd146f038512c69599
Arkadiusz Chmura <arekchmura@gmail.com>
2019-02-25 19:30:03 +0100

    ViewModel for events

---
+++ app/src/main/java/com/arkadiusz/dayscounter/viewmodels/EventsViewModel.kt
@@ -0,0 +1,49 @@
+package com.arkadiusz.dayscounter.viewmodels
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import com.arkadiusz.dayscounter.model.Event
+import com.arkadiusz.dayscounter.repositories.DatabaseProvider
+import com.arkadiusz.dayscounter.utils.RemindersUtils
+import io.realm.RealmResults
+
+class EventsViewModel : ViewModel() {
+
+    private val databaseRepository = DatabaseProvider.provideRepository()
+
+    private lateinit var eventsPastList: RealmResults<Event>
+    private lateinit var eventsFutureList: RealmResults<Event>
+
+    fun init(sortType: String, context: Context?) {
+        if (!::eventsFutureList.isInitialized && !::eventsPastList.isInitialized) {
+
+            databaseRepository.syncToCloud(context)
+
+            eventsPastList = databaseRepository.getPastEvents()
+            eventsFutureList = databaseRepository.getFutureEvents()
+
+            sortEventsList(sortType)
+        }
+    }
+
+    private fun sortEventsList(sortType: String) {
+        when (sortType) {
+            "date_desc" -> {
+                eventsPastList = databaseRepository.sortEventsDateAsc(eventsPastList)
+                eventsFutureList = databaseRepository.sortEventsDateDesc(eventsFutureList)
+            }
+            "date_asc" -> {
+                eventsPastList = databaseRepository.sortEventsDateDesc(eventsPastList)
+                eventsFutureList = databaseRepository.sortEventsDateAsc(eventsFutureList)
+            }
+        }
+    }
+
+    fun deleteEventAndRelatedReminder(context: Context, event: Event) {
+        RemindersUtils.deleteReminder(context, event)
+        databaseRepository.deleteEventFromDatabase(event.id)
+    }
+
+    fun getPastEvents(): RealmResults<Event> = eventsPastList
+    fun getFutureEvents(): RealmResults<Event> = eventsFutureList
+}
