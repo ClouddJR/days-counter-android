a318aae21cac28b401d539f1349ede0c8be95193
Arkadiusz Chmura <arekchmura@gmail.com>
2019-02-25 18:52:39 +0100

    Firestore initialization in Application class

--- app/src/main/java/com/arkadiusz/dayscounter/DaysCounterApp.kt
+++ app/src/main/java/com/arkadiusz/dayscounter/DaysCounterApp.kt
@@ -4,7 +4,10 @@ import android.app.Application
 import android.os.Build
 import com.arkadiusz.dayscounter.repositories.DatabaseRepository
 import com.arkadiusz.dayscounter.utils.NotificationUtils
-import com.google.firebase.database.FirebaseDatabase
+import com.google.android.gms.ads.MobileAds
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
+
 
 /**
  * Created by Arkadiusz on 14.03.2018
@@ -15,16 +18,20 @@ class DaysCounterApp : Application() {
     override fun onCreate() {
         super.onCreate()
         initializeRealm()
-        initializeFirebaseOfflinePersistence()
         createNotificationChannelForReminders()
+
+        MobileAds.initialize(this, "ca-app-pub-4098342918729972~7968062049")
+
+        val firestore = FirebaseFirestore.getInstance()
+        val settings = FirebaseFirestoreSettings.Builder()
+                .setTimestampsInSnapshotsEnabled(false)
+                .setPersistenceEnabled(false)
+                .build()
+        firestore.firestoreSettings = settings
     }
 
     private fun initializeRealm() {
         DatabaseRepository.RealmInitializer.initRealm(this)
-    }
-
-    private fun initializeFirebaseOfflinePersistence() {
-        FirebaseDatabase.getInstance().setPersistenceEnabled(true)
     }
 
     private fun createNotificationChannelForReminders() {
