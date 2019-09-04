f5182ca7e66e5246337c4aafee952483574042bc
Arkadiusz Chmura <arekchmura@gmail.com>
2019-09-04 19:10:15 +0200

    Change repository variable to be local and not singleton
    When using singleton version of the DatabaseRepository, realm instance that
    was created on initialization was tied to the background thread that the
    WidgetUpdateWorker operates on. Given that the singleton instance is then
    used inside the app, the same realm instance resides which can't be used
    on the main thread as a result.

--- app/src/main/java/com/arkadiusz/dayscounter/Provider/WidgetUpdateWorker.kt
+++ app/src/main/java/com/arkadiusz/dayscounter/Provider/WidgetUpdateWorker.kt
@@ -6,13 +6,17 @@ import android.content.Intent
 import androidx.work.Data
 import androidx.work.Worker
 import androidx.work.WorkerParameters
-import com.arkadiusz.dayscounter.repositories.DatabaseProvider
+import com.arkadiusz.dayscounter.repositories.DatabaseRepository
 
 class WidgetUpdateWorker(private val appContext: Context, workerParams: WorkerParameters) :
         Worker(appContext, workerParams) {
 
+    companion object {
+        const val PERIODIC_WORK_WIDGET_UPDATE = "widget_update"
+    }
+
     override fun doWork(): Result {
-        val eventsWithWidget = DatabaseProvider.provideRepository().getEventsWithWidgets()
+        val eventsWithWidget = DatabaseRepository().getEventsWithWidgets()
         val widgetIds = eventsWithWidget.map { it.widgetID }.toIntArray()
 
         val intent = Intent(AppWidgetManager.ACTION_APPWIDGET_UPDATE, null, appContext,
