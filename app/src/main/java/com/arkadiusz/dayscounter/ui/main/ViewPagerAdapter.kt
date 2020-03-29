4f33247997ab48e3850d0a5484ab12a6d43fa812
Arkadiusz Chmura <arekchmura@gmail.com>
2020-03-29 13:37:57 +0200

    Remove fragments list from ViewPagerAdapter

--- app/src/main/java/com/arkadiusz/dayscounter/ui/main/ViewPagerAdapter.kt
+++ app/src/main/java/com/arkadiusz/dayscounter/ui/main/ViewPagerAdapter.kt
@@ -1,28 +1,36 @@
 package com.arkadiusz.dayscounter.ui.main
 
+import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.arkadiusz.dayscounter.R
+import com.arkadiusz.dayscounter.ui.events.FutureEventsFragment
+import com.arkadiusz.dayscounter.ui.events.PastEventsFragment
 
-class ViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
+class ViewPagerAdapter(private val context: Context, manager: FragmentManager,
+                       private val defaultFragment: String)
+    : FragmentPagerAdapter(manager) {
 
-    private val fragmentList = mutableListOf<Fragment>()
-    private val fragmentTitleList = mutableListOf<String>()
-
-    override fun getItem(position: Int): Fragment {
-        return fragmentList[position]
+    override fun getItem(position: Int) = when (position) {
+        0 -> if (defaultFragment == context.getString(R.string.main_activity_left_tab))
+            PastEventsFragment() else
+            FutureEventsFragment()
+        1 -> if (defaultFragment == context.getString(R.string.main_activity_left_tab))
+            FutureEventsFragment() else
+            PastEventsFragment()
+        else -> throw IllegalStateException("Unexpected position $position")
     }
 
-    override fun getCount(): Int {
-        return fragmentList.size
-    }
+    override fun getCount() = 2
 
-    override fun getPageTitle(position: Int): CharSequence? {
-        return fragmentTitleList[position]
-    }
-
-    fun addFragment(fragment: Fragment, title: String) {
-        fragmentList.add(fragment)
-        fragmentTitleList.add(title)
+    override fun getPageTitle(position: Int): CharSequence = when (position) {
+        0 -> if (defaultFragment == context.getString(R.string.main_activity_left_tab))
+            context.getString(R.string.main_activity_left_tab) else
+            context.getString(R.string.main_activity_right_tab)
+        1 -> if (defaultFragment == context.getString(R.string.main_activity_left_tab))
+            context.getString(R.string.main_activity_right_tab) else
+            context.getString(R.string.main_activity_left_tab)
+        else -> throw IllegalStateException("Unexpected position $position")
     }
 }
