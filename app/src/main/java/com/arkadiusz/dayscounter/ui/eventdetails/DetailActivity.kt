c5e900054e39aa511e157bf48ead0925d51f24b3
Arkadiusz Chmura <arekchmura@gmail.com>
2021-09-22 14:13:28 +0200

    Fix crash with uninitialized property

--- app/src/main/java/com/arkadiusz/dayscounter/ui/eventdetails/DetailActivity.kt
+++ app/src/main/java/com/arkadiusz/dayscounter/ui/eventdetails/DetailActivity.kt
@@ -11,14 +11,14 @@ import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.arkadiusz.dayscounter.R
 import com.arkadiusz.dayscounter.data.model.Event
 import com.arkadiusz.dayscounter.ui.addeditevent.EditActivity
-import com.arkadiusz.dayscounter.util.ViewModelUtils.getViewModel
-import com.arkadiusz.dayscounter.util.PreferenceUtils.defaultPrefs
-import com.arkadiusz.dayscounter.util.PreferenceUtils.get
 import com.arkadiusz.dayscounter.util.DateUtils.calculateDate
 import com.arkadiusz.dayscounter.util.DateUtils.formatDate
 import com.arkadiusz.dayscounter.util.DateUtils.formatDateAccordingToSettings
 import com.arkadiusz.dayscounter.util.DateUtils.formatTime
+import com.arkadiusz.dayscounter.util.PreferenceUtils.defaultPrefs
+import com.arkadiusz.dayscounter.util.PreferenceUtils.get
 import com.arkadiusz.dayscounter.util.ThemeUtils
+import com.arkadiusz.dayscounter.util.ViewModelUtils.getViewModel
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.google.android.gms.ads.AdRequest
@@ -31,7 +31,7 @@ class DetailActivity : AppCompatActivity() {
 
     private lateinit var viewModel: DetailActivityViewModel
 
-    private lateinit var passedEvent: Event
+    private var passedEvent: Event? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(ThemeUtils.getThemeFromPreferences(false, this))
@@ -39,20 +39,21 @@ class DetailActivity : AppCompatActivity() {
         setContentView(R.layout.activity_detail)
 
         initViewModel()
-        val wasSuccessfullyFetched = fetchPassedEvent()
-        if (!wasSuccessfullyFetched) {
+        fetchPassedEvent()
+
+        if (passedEvent != null) {
+            setStatusBarColor()
+            setUpToolbar()
+            cancelNotification()
+            displayImage()
+            fillMainSection()
+            fillAboutSection()
+            fillReminderSection()
+            fillRepetitionSection()
+            displayAd()
+        } else {
             displayToastAndFinishActivity()
-            return
         }
-        setStatusBarColor()
-        setUpToolbar()
-        cancelNotification()
-        displayImage()
-        fillMainSection()
-        fillAboutSection()
-        fillReminderSection()
-        fillRepetitionSection()
-        displayAd()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -63,17 +64,17 @@ class DetailActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.action_edit -> {
-                startActivity<EditActivity>("eventId" to passedEvent.id)
+                startActivity<EditActivity>("eventId" to passedEvent!!.id)
                 finish()
             }
 
             R.id.action_delete -> {
                 alert(getString(R.string.fragment_delete_dialog_question)) {
-                    positiveButton(android.R.string.yes) {
-                        viewModel.deleteEvent(passedEvent.id)
+                    positiveButton(android.R.string.ok) {
+                        viewModel.deleteEvent(passedEvent!!.id)
                         finish()
                     }
-                    negativeButton(android.R.string.no) {}
+                    negativeButton(android.R.string.cancel) {}
                 }.show()
             }
         }
@@ -85,12 +86,8 @@ class DetailActivity : AppCompatActivity() {
         viewModel = getViewModel(this)
     }
 
-    private fun fetchPassedEvent(): Boolean {
-        val event = viewModel.getEventById(intent.getStringExtra("event_id")!!)
-        if (event != null) {
-            passedEvent = event
-        }
-        return event != null
+    private fun fetchPassedEvent() {
+        passedEvent = viewModel.getEventById(intent.getStringExtra("event_id")!!)
     }
 
     private fun displayToastAndFinishActivity() {
@@ -99,8 +96,8 @@ class DetailActivity : AppCompatActivity() {
     }
 
     private fun setStatusBarColor() {
-        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-        window.statusBarColor = ContextCompat.getColor(this, android.R.color.transparent)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.half_translucent)
     }
 
     private fun setUpToolbar() {
@@ -113,7 +110,7 @@ class DetailActivity : AppCompatActivity() {
         val isComingFromNotification = intent.getStringExtra("notificationClick")
 
         if (isComingFromNotification != null && isComingFromNotification == "clicked") {
-            notificationManager.cancel(passedEvent.id.hashCode())
+            notificationManager.cancel(passedEvent!!.id.hashCode())
         }
     }
 
@@ -130,23 +127,23 @@ class DetailActivity : AppCompatActivity() {
         circularProgressDrawable.start()
 
         when {
-            passedEvent.imageColor != 0 -> {
+            passedEvent!!.imageColor != 0 -> {
                 eventImage.setImageDrawable(null)
-                eventImage.backgroundColor = passedEvent.imageColor
+                eventImage.backgroundColor = passedEvent!!.imageColor
             }
-            passedEvent.imageID == 0 -> {
+            passedEvent!!.imageID == 0 -> {
                 when {
-                    File(passedEvent.image).exists() ->
+                    File(passedEvent!!.image).exists() ->
                         Glide.with(this)
-                            .load(passedEvent.image)
+                            .load(passedEvent!!.image)
                             .diskCacheStrategy(DiskCacheStrategy.NONE)
                             .skipMemoryCache(true)
                             .into(eventImage)
-                    passedEvent.imageCloudPath.isNotEmpty() -> {
+                    passedEvent!!.imageCloudPath.isNotEmpty() -> {
                         Glide.with(this)
                             .load(
                                 FirebaseStorage.getInstance()
-                                    .getReference(passedEvent.imageCloudPath)
+                                    .getReference(passedEvent!!.imageCloudPath)
                             )
                             .placeholder(circularProgressDrawable)
                             .into(eventImage)
@@ -156,36 +153,36 @@ class DetailActivity : AppCompatActivity() {
                     }
                 }
             }
-            else -> Glide.with(this).load(passedEvent.imageID).into(eventImage)
+            else -> Glide.with(this).load(passedEvent!!.imageID).into(eventImage)
         }
     }
 
     private fun fillMainSection() {
         val counterText = calculateDate(
-            passedEvent.date,
-            passedEvent.formatYearsSelected,
-            passedEvent.formatMonthsSelected,
-            passedEvent.formatWeeksSelected,
-            passedEvent.formatDaysSelected, this
+            passedEvent!!.date,
+            passedEvent!!.formatYearsSelected,
+            passedEvent!!.formatMonthsSelected,
+            passedEvent!!.formatWeeksSelected,
+            passedEvent!!.formatDaysSelected, this
         )
         eventCalculateText.text = counterText
-        eventTitle.text = passedEvent.name
+        eventTitle.text = passedEvent!!.name
     }
 
     private fun fillAboutSection() {
         dateText.text = formatDateAccordingToSettings(
-            passedEvent.date,
+            passedEvent!!.date,
             defaultPrefs(this)["dateFormat"] ?: ""
         )
-        if (passedEvent.description.isNotEmpty()) {
-            descriptionText.text = passedEvent.description
+        if (passedEvent!!.description.isNotEmpty()) {
+            descriptionText.text = passedEvent!!.description
         } else {
             descriptionText.visibility = View.GONE
         }
     }
 
     private fun fillRepetitionSection() {
-        repeatSectionText.text = when (passedEvent.repeat) {
+        repeatSectionText.text = when (passedEvent!!.repeat) {
             "0" -> getString(R.string.detail_once)
             "1" -> getString(R.string.detail_daily)
             "2" -> getString(R.string.detail_weekly)
@@ -196,21 +193,21 @@ class DetailActivity : AppCompatActivity() {
     }
 
     private fun fillReminderSection() {
-        if (passedEvent.reminderYear != 0) {
+        if (passedEvent!!.reminderYear != 0) {
             val reminderDate = "${
                 formatDateAccordingToSettings(
                     formatDate(
-                        passedEvent.reminderYear,
-                        passedEvent.reminderMonth,
-                        passedEvent.reminderDay
+                        passedEvent!!.reminderYear,
+                        passedEvent!!.reminderMonth,
+                        passedEvent!!.reminderDay
                     ),
                     defaultPrefs(this)["dateFormat"] ?: ""
                 )
             } " +
-                    formatTime(passedEvent.reminderHour, passedEvent.reminderMinute)
+                    formatTime(passedEvent!!.reminderHour, passedEvent!!.reminderMinute)
 
             reminderSectionText.text = reminderDate
-            reminderDescriptionText.text = passedEvent.notificationText
+            reminderDescriptionText.text = passedEvent!!.notificationText
         } else {
             reminderDescriptionText.visibility = View.GONE
         }
