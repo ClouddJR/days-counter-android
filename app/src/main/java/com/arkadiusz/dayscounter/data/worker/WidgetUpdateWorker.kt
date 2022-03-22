6ef919b233b5a9c9e5668e8a42871e00b50b6488
Arkadiusz Chmura <arekchmura@gmail.com>
2022-03-22 20:49:05 +0100

    Manually construct DatabaseRepository because of Realm thread weirdness

--- app/src/main/java/com/arkadiusz/dayscounter/data/worker/WidgetUpdateWorker.kt
+++ app/src/main/java/com/arkadiusz/dayscounter/data/worker/WidgetUpdateWorker.kt
@@ -8,7 +8,11 @@ import androidx.work.Data
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.arkadiusz.dayscounter.Provider.AppWidgetProvider
+import com.arkadiusz.dayscounter.data.local.LocalDatabase
+import com.arkadiusz.dayscounter.data.remote.RemoteDatabase
 import com.arkadiusz.dayscounter.data.repository.DatabaseRepository
+import com.arkadiusz.dayscounter.data.repository.UserRepository
+import com.google.firebase.firestore.FirebaseFirestore
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 
@@ -16,7 +20,9 @@ import dagger.assisted.AssistedInject
 class WidgetUpdateWorker @AssistedInject constructor(
     @Assisted private val appContext: Context,
     @Assisted workerParams: WorkerParameters,
-    private val databaseRepository: DatabaseRepository,
+    private val userRepository: UserRepository,
+    private val firestore: FirebaseFirestore,
+    private val remoteDatabase: RemoteDatabase,
 ) : Worker(appContext, workerParams) {
 
     companion object {
@@ -24,6 +30,9 @@ class WidgetUpdateWorker @AssistedInject constructor(
     }
 
     override fun doWork(): Result {
+        val localDatabase = LocalDatabase(firestore)
+        val databaseRepository = DatabaseRepository(userRepository, localDatabase, remoteDatabase)
+
         val eventsWithWidget = databaseRepository.getEventsWithWidgets()
         val widgetIds = eventsWithWidget.map { it.widgetID }.toIntArray()
 
